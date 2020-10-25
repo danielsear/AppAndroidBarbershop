63f28d00393ee7baf1a98ef9c57f05c3e8138dd2
Danielsear <danielandradeseara@hotmail.com>
2020-10-25 08:18:44 -0300

    customizing login(SigIn) screen and create component SignInput and adding onchange functionality in the email and password fields

--- src/screens/SigIn/index.js
+++ src/screens/SigIn/index.js
@@ -1,11 +1,53 @@
-import React from "react";
-import { Text } from "react-native";
-import { Container } from "./styles";
+import React, { useState } from "react";
+
+import BarbeLogo from "../../assets/barber.svg";
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+  SingMessageButton,
+  SingMessageButtonText,
+  SingMessageButtonTextBold,
+} from "./styles";
+import InputArea from "../../components/SignInput";
+import SignInput from "../../components/SignInput";
+import EmailIcon from "../../assets/email.svg";
+import LockIcon from "../../assets/lock.svg";
 
 export default () => {
+  const [emailField, setEmailField] = useState("");
+  const [passwordField, setPasswordlField] = useState("");
+
   return (
     <Container>
-      <Text>SingUp</Text>
+      <BarbeLogo width="100%" height="160" />
+
+      <InputArea>
+        <SignInput
+          IconSvg={EmailIcon}
+          placeholder="Digite seu email"
+          value={emailField}
+          onChangeText={(text) => setEmailField(text)}
+        />
+        <SignInput
+          IconSvg={LockIcon}
+          placeholder="Digite sua senha"
+          value={passwordField}
+          onChangeText={(text) => setPasswordlField(text)}
+          password={true}
+        />
+
+        <CustomButton>
+          <CustomButtonText>LOGIN</CustomButtonText>
+        </CustomButton>
+      </InputArea>
+      <SingMessageButton>
+        <SingMessageButtonText>
+          Ainda não possui uma conta?
+        </SingMessageButtonText>
+        <SingMessageButtonTextBold>Cadastre-se</SingMessageButtonTextBold>
+      </SingMessageButton>
     </Container>
   );
 };
