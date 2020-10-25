b52f2192c95a4b43fa7af777d521b0fc48f3330b
Danielsear <danielandradeseara@hotmail.com>
2020-10-25 06:22:05 -0300

    created canvas sketch

--- src/screens/SigUp/index.js
+++ src/screens/SigUp/index.js
@@ -5,7 +5,7 @@ import { Container } from "./styles";
 export default () => {
   return (
     <Container>
-      <Text>SigIn</Text>
+      <Text>SigUp</Text>
     </Container>
   );
 };
--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { createNavigatorFactory } from "@react-navigation/native";
-
+import Preload from "../screens/Preload";
+import SigIn from "../screens/SigIn";
+import SigUp from "../screens/SigUp";
 const Stack = createNavigatorFactory();
 
 export default () => (
   <Stack.Navigator>
+    screenOptions=
+    {{
+      headerShow: false,
+    }}
     <Stack.Screen name="Preload" component={Preload} />
     <Stack.Screen name="SigIn" component={SigIn} />
     <Stack.Screen name="SigUp" component={SigUp} />
