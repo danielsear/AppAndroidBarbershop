8510f411db9854d93f7ca59027fe7961bf4f5fc7
Danielsear <danielandradeseara@hotmail.com>
2020-10-25 14:34:24 -0300

    validating the token

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,16 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import { Container, LoadingIcon } from "./styles";
 import BarbeLogo from "../../assets/barber.svg";
 import AsyncStorage from "@react-native-community/async-storage";
-import {useNavigation} from '@react-navigation/native'
+import {useNavigation} from '@react-navigation/native';
+import Api from '../../Api';
+import AsyncStorage from "@react-native-community/async-storage";
+import UseContext from "../../contexts/UserContex";
 
 export default () => {
+  const { dispatch: userDispatch } = useContext(UseContext);
   const navigation = useNavigation();
   useEffect(() => {
     const checkToken = (async) => {
       const token = await AsyncStorage.getItem('token');
-      if(token !== null){
+      if(token){
         //validar o toke
+        let res = await Api.checkToken(token);
+        //verificar se ele retornou algum token
+        if(res.token){
+
+           //token mudado bora
+
+        //salvar o usuario no AsyncStorage
+        await AsyncStorage.setItem("token", res.token);
+        //salvar o avatar do usuario no context
+        userDispatch({
+          type: "setAvatar",
+          payload: {
+            avatar: res.data.avatar,
+          },
+        });
+        //manda o usuario sem retorno para o MainTab
+        navigation.reset({
+          routes: [{ name: "MainTab" }],
+        });
+
+        }else{
+          navigation.navigate('SigIn');
+        }
       
       }else{
         //manda direto pro login
