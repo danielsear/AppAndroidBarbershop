360809e7aaa140b2b498958af035092c4376d076
Danielsear <danielandradeseara@hotmail.com>
2020-10-25 16:02:59 -0300

    customized the avatar in the account field if there is an avatar in the user registration

--- src/components/customTagBar.js
+++ src/components/customTagBar.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components/native";
 import HomeIcon from "../assets/home.svg";
 import SearchIcon from "../assets/search.svg";
 import TodayIcon from "../assets/today.svg";
 import FavoriteIcon from "../assets/favorite.svg";
 import AccountIcon from "../assets/account.svg";
+import UseContext from "../contexts/UserContex";
 
 const TabArea = styled.View`
     height:60px,
@@ -26,8 +27,15 @@ const TabItemCenter = styled.TouchableOpacity`
   border: 3px solid #4eadbe;
   margin-top: -20px;
 `;
+const AvatarIcon = styled.Image`
+  width: 24px;
+  height: 24px;
+  border-radius: 12px;
+`;
 
 export default (state, navigation) => {
+  const { state: user } = useContext(UseContext);
+
   const goTo = (screenName) => {
     navigation.navigate(screenName);
   };
@@ -61,12 +69,19 @@ export default (state, navigation) => {
         style={{ opacity: state.index === 3 ? 1 : 0.5 }}
         onPress={() => goTo("Profile")}
       >
-        <AccountIcon
-          style={{ opacity: state.index === 4 ? 1 : 0.5 }}
-          width="24"
-          height="24"
-          fill="#ffffff"
-        />
+        {user.avatar != "" ? (
+          <AvatarIcon
+            source={{ uri: user.avatar }}
+            style={{ border: state.index === 4 ? "none" : "1px solid #fff" }}
+          />
+        ) : (
+          <AccountIcon
+            style={{ opacity: state.index === 4 ? 1 : 0.5 }}
+            width="24"
+            height="24"
+            fill="#ffffff"
+          />
+        )}
       </TabItem>
     </TabArea>
   );
@@ -74,3 +89,4 @@ export default (state, navigation) => {
 
 // a propriedade state mostra qual tela estara ativa
 //style={{ opacity: state.index === 4 ? 1 : 0.5 }}se o estado da tela4 for igual 0 o opacity=1 caso contrario opacity=0.5
+//caso user.avatar existe use o AvatarIcon , do contrario AccountIcon
