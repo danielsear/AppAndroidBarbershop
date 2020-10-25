d7cfacd4f8aec1e5749cab1d64603614aa944239
Danielsear <danielandradeseara@hotmail.com>
2020-10-25 17:07:53 -0300

    stylizing home field

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,46 @@
 import React from "react";
-import { Container } from "./styles";
-import { Text } from "react-native";
+import SearchIcon from "../../assets/search.svg";
+import MyLocationIcon from "../../assets/my_location.svg";
+import { useNavigation } from "@react-navigation/native";
+
+import {
+  Container,
+  Scroller,
+  HeaderArea,
+  HeaderTitle,
+  SearchButton,
+  LocationArea,
+  LocationInput,
+  LocationFinder,
+} from "./styles";
 
 export default () => {
+  const navigation = useNavigation();
   return (
     <Container>
-      <Text>Home</Text>
+      <Scroller>
+        <HeaderArea>
+          <HeaderTitle numberOfLines={2}>
+            Encontre o seu barbeiro favorito
+          </HeaderTitle>
+          <SearchButton onPress={() => navigation.navigate("Search")}>
+            <SearchIcon width="26" height="26" fill="#ffffff" />
+          </SearchButton>
+        </HeaderArea>
+
+        <LocationArea>
+          <LocationInput
+            placeholder="Onde você está?"
+            placeholderTextColor="#ffffff"
+          />
+          <LocationFinder>
+            <MyLocationIcon width="24" height="24" fill="#ffffff" />
+          </LocationFinder>
+        </LocationArea>
+      </Scroller>
     </Container>
   );
 };
+
+// conceito de refreshControl, de arrastar pra baixo e atualizar o campo
+//numberOfLines={2}  duas linhas
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,4 +1,45 @@
 import React from "react";
 import styled from "styled-components/native";
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: #63c2d1;
+`;
+
+export default Scroller = styled.ScrollView`
+        flex: 1
+        padding: 20px;
+`;
+export default HeaderArea = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+export default HeaderTitle = styled.Text`
+  width: 250px;
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff;
+`;
+export default SearchButton = styled.TouchableOpacity`
+  width: 26px;
+  height: 26px;
+`;
+export default LocationArea = styled.View`
+  background-color: #4eadbe;
+  height: 60px;
+  border-radius: 30px;
+  flex-direction: row;
+  align-items: center;
+  padding: 0 20px 0 20px;
+  margin-top: 30px;
+`;
+export default LocationInput = styled.TextInput`
+  flex: 1;
+  font-size: 16px;
+  color: #fff;
+`;
+export default LocationFinder = styled.TouchableOpacity`
+  width: 24px;
+  height: 24px;
+`;
